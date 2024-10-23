b6f14a4c7a77cc1970e36d2b359a559615a1f482
kidchang93 <lck930721@gmail.com>
2024-10-23 14:19:37 +0900

    feat: sink-connector 설정

---
+++ src/main/java/kr/co/simplesinkconnector/SimpleSinkConnectorApplication.java
@@ -0,0 +1,13 @@
+package kr.co.simplesinkconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSinkConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSinkConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/simplesinkconnector/SimpleSinkConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.simplesinkconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSinkConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
